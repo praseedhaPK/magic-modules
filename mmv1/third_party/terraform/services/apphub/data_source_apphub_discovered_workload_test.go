50aa3f439697996c995a7d7a625a38e09a403754
Praseedha Praveen Kalbhavi <praseedha@google.com>
2024-03-11 09:15:52 +0000

    Updated logic

--- mmv1/third_party/terraform/services/apphub/data_source_apphub_discovered_workload_test.go
+++ mmv1/third_party/terraform/services/apphub/data_source_apphub_discovered_workload_test.go
@@ -11,6 +11,7 @@ func TestAccDataSourceApphubDiscoveredWorkload_basic(t *testing.T) {
     t.Parallel()
 
     context := map[string]interface{}{
+    	"org_id":        envvar.GetTestOrgFromEnv(t),
     	"random_suffix": acctest.RandString(t, 10),
     }
 
@@ -27,11 +28,40 @@ func TestAccDataSourceApphubDiscoveredWorkload_basic(t *testing.T) {
 
 func testDataSourceApphubDiscoveredWorkload_basic(context map[string]interface{}) string {
     return acctest.Nprintf(`
+resource "google_project" "service_project" {
+	project_id ="apphub-service-project-%{random_suffix}"
+	name = "Service Project"
+	org_id = "%{org_id}"
+}
+
+resource "time_sleep" "wait_120s_for_service_project" {
+  depends_on = [google_project.service_project]
+  create_duration = "120s"
+}
+
+# Enable Compute API
+resource "google_project_service" "compute_service_project" {
+  project = google_project.service_project.project_id
+  service = "compute.googleapis.com"
+  depends_on = [time_sleep.wait_120s_for_service_project]
+}
+
+resource "time_sleep" "wait_120s_for_compute_api" {
+  depends_on = [google_project_service.compute_service_project]
+  create_duration = "120s"
+}
+
+resource "google_apphub_service_project_attachment" "service_project_attachment" {
+  service_project_attachment_id = google_project.service_project.project_id
+  depends_on = [time_sleep.wait_120s_for_service_project]
+}
+    
 data "google_apphub_discovered_workload" "catalog-workload" {
   provider = google
   location = "us-central1"
-  for_each= data.google_compute_region_instance_group.ig.instances
-  workload_uri = "//compute.googleapis.com/${each.value.attributes.id}"
+  count=0
+  workload_uri = "//compute.googleapis.com/${data.google_compute_region_instance_group.ig.instances[count.index].attributes.id}"
+  depends_on = [google_apphub_service_project_attachment.service_project_attachment]
 }
 
 data "google_compute_region_instance_group" "ig" {
@@ -41,14 +71,15 @@ data "google_compute_region_instance_group" "ig" {
 # VPC network
 resource "google_compute_network" "ilb_network" {
   name                    = "l7-ilb-network-%{random_suffix}"
-  project                 = "tf-project-5-416803"
+  project                 = google_project.service_project.project_id
   auto_create_subnetworks = false
+  depends_on = [time_sleep.wait_120s_for_compute_api]
 }
 
 # backend subnet
 resource "google_compute_subnetwork" "ilb_subnet" {
   name          = "l7-ilb-subnetwork-%{random_suffix}"
-  project       = "tf-project-5-416803"
+  project       = google_project.service_project.project_id
   ip_cidr_range = "10.0.1.0/24"
   region        = "us-central1"
   network       = google_compute_network.ilb_network.id
@@ -58,7 +89,7 @@ resource "google_compute_subnetwork" "ilb_subnet" {
 # instance template
 resource "google_compute_instance_template" "instance_template" {
   name         = "l7-ilb-mig-template-%{random_suffix}"
-  project               = "tf-project-5-416803"
+  project               = google_project.service_project.project_id
   machine_type = "e2-small"
   tags         = ["http-server"]
   network_interface {
@@ -100,7 +131,7 @@ resource "google_compute_instance_template" "instance_template" {
 }
 resource "google_compute_region_instance_group_manager" "mig" {
   name     = "l7-ilb-mig1-%{random_suffix}"
-  project               = "tf-project-5-416803"
+  project               = google_project.service_project.project_id
   region   = "us-central1"
   version {
     instance_template = google_compute_instance_template.instance_template.id
